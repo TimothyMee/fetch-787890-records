6941c034805784ed62b4c19570238ccab6ffcdeb
Timothy Fadayini <tf@instamaterial.com>
2021-02-01 09:07:36 +0100

    added more coverage to the testing

--- tests/fetch-records.spec.ts
+++ tests/fetch-records.spec.ts
@@ -25,3 +25,39 @@ describe("fetch record", () => {
         done()
       })
 });
+
+describe("fetch record", () => {
+    it('posts to the fetch endpoint with bad date string', async done => {
+        const response = await request.post('/')
+            .send({
+                "startDate": "badDate",
+                "endDate": "2016-02-01",
+                "minCount": 4970,
+                "maxCount": 4980
+            });
+      
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('msg')
+        expect(response.body.msg).toBe('Invalid dates. Check start and end date')
+        expect(response.body.code).toBe(400)
+        done()
+      })
+});
+
+describe("fetch record", () => {
+    it('posts to the fetch endpoint without body parameters', async done => {
+        const response = await request.post('/')
+            .send({
+                "endDate": "2016-02-01",
+                "minCount": 4970,
+                "maxCount": 4980
+            });
+      
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('msg')
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.msg).toBe('Error in request parameter(s)')
+        expect(response.body.code).toBe(400)
+        done()
+      })
+});
